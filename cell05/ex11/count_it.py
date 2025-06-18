8f488ae501846f9d6390ab848266feb79932299d
nwatjana <nxnneill@gmail.com>
2025-06-18 15:09:11 +0000

    submit5.11

---
+++ cell05/ex11/count_it.py
@@ -0,0 +1,11 @@
+import sys
+def main():
+    args = sys.argv[1:]
+    if not args:
+        print("none")
+        return
+    print(f"parameters: {len(args)}")
+    for arg in args:
+        print(f"{arg}: {len(arg)}")
+if __name__ == "__main__":
+    main()
